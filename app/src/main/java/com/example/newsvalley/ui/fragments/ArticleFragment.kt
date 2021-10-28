1dbe82bb4c446d6d6e17b3581e6168b6dd9b9cca
Jill Jani <jaldhi2000@gmail.com>
2021-10-28 23:45:34 +0530

    Setup Navigation with bottom nav menu

---
+++ app/src/main/java/com/example/newsvalley/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newsvalley.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newsvalley.R
+
+class ArticleFragment: Fragment(R.layout.fragment_article) {
+}
---
+++ app/src/main/java/com/example/newsvalley/ui/fragments/ExploreNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newsvalley.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newsvalley.R
+
+class ExploreNewsFragment: Fragment(R.layout.fragment_explore_news) {
+}
